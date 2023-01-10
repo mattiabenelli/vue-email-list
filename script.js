b6e947c2785edc5ebff378e2122d7924be4f8ce1
mattiabenelli <benellimattia0@gmail.com>
2023-01-10 09:50:25 +0100

    bonus done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ createApp({
         }
     },
     mounted() {
-        for(let i = 0; i < 9; i++){
+        for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
             this.email = result.data.response
             this.mailArray.push(this.email)
